752c28d843dfcceda94a62ef6c057cd94f0894c9
phatrickgomes <gomesphatrick@gmail.com>
2025-08-04 09:33:33 -0300

    player
    mudei algumas coisas no codigo como tirar o pulo

--- player.gd
+++ player.gd
@@ -13,7 +13,6 @@ var objeto_selecionado
 var forca_braco = 4 
 
 const SPEED = 10.0
-const JUMP_VELOCITY = 5.5
 const SENSIBILIDADE = 0.003
 
 var mouse = Vector2()
@@ -80,8 +79,6 @@ func _physics_process(delta: float) -> void:
 		##apenas a movimentaçao
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (camera_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
