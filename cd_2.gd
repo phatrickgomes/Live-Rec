87cacb8486edd2a973987037fc55f4e001107a29
phatrick123 <gomesphatrick@gmail.com>
2025-08-16 05:39:05 -0300

    tentando fazer a fita
    ta dificil pra krl o rigid body ta reconhendo tudo como um so ai a fita funciona em qualquer ocasiao

--- cd_2.gd
+++ cd_2.gd
@@ -1,13 +1,8 @@
 extends RigidBody3D
-
 var player  
-
 @onready var cd = $cd
 
-
 var selecionado = false
-
-
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	add_to_group("pegavel") 
--- jogo_12.gd
+++ jogo_12.gd
@@ -5,10 +5,9 @@ var player
 
 var selecionado = false
 
-
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	add_to_group("pegavel") 
-
+	
 func _set_selected(object):
 	selecionado = self == object
