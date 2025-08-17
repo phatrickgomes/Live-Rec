ddc19f5eb081726dc55f498095324f9ca828a1e1
phatrick123 <161028190+phatrick123@users.noreply.github.com>
2025-08-17 03:58:32 -0300

    refiz o jogo 2d
    o jogo2d nao tinha funfado tiver que refazer

--- Assets/Script/control.gd
+++ Assets/Script/control.gd
@@ -4,7 +4,7 @@ extends Control
 
 var labirinto = preload("res://Assets/Scenes/labirinto.tscn")
 var jamv = preload("res://Assets/Scenes/sprite_2d.tscn")
-const MAIN_SCENE = ("res://Assets/Scenes/main_scene.tscn")
+#var jogo2d = preload("res://Assets/Scenes/main_scene.tscn")
 
 func _on_button_pressed() -> void:
 	var player = PlayerManager.get_current_player()
@@ -22,14 +22,11 @@ func _on_line_edit_text_submitted(new_text: String) -> void:
 			child.queue_free()
 		var jamv_inst = jamv.instantiate()
 		sub_viewport.add_child(jamv_inst)
-		
-		# Registrar o jogador interno
 		var internal_player = jamv_inst.find_child("SparkyGlory")
 		if internal_player:
 			PlayerManager.register_internal_player(internal_player)
 		else:
 			printerr("Jogador interno não encontrado!")
-		
 		print("CESAR PATROCINA NOIS")
 
 func _on_jogo_2_pressed():
@@ -37,14 +34,13 @@ func _on_jogo_2_pressed():
 	if player and player.objeto_selecionado != null and player.objeto_selecionado.is_in_group("fita"):
 		for child in sub_viewport.get_children():
 			child.queue_free()
+		#var maze_inst = jogo2d.instantiate()
+		#sub_viewport.add_child(maze_inst)
 	else:
 		print("voce precisa da fita")
 
 func _on_sair_do_jogo_pressed():
 	PlayerManager.clear_internal_player()
-	
-	# Limpa a viewport
 	for child in sub_viewport.get_children():
 		child.queue_free()
-	
 	print("Saiu do jogo interno")
---
+++ cd1.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+var player  
+@onready var fita = $fita
+
+var selecionado = false
+
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	add_to_group("pegavel") 
+
+func _set_selected(object):
+	selecionado = self == object
