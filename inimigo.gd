8df7f4d9afc336c748e4887ad054a4785d830e0b
phatrick123 <161028190+phatrick123@users.noreply.github.com>
2025-08-19 22:15:25 -0300

    fazendo ring
    bora que bora

---
+++ inimigo.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+@onready var vida_inimigo = $progress_inimigo
+
+@onready var anima = $AnimationPlayer
+
+
+
+var max_health = 100
+var current_health = max_health
+var damage_amount = 5  ##dano que cada soco causa
+
+func _ready():
+	##como começa a barra de vida
+	vida_inimigo.max_value = max_health
+	vida_inimigo.value = current_health
+	update_health_bar()
+
+func take_damage(damage):
+	current_health -= damage
+	current_health = max(0, current_health) 
+	update_health_bar()
+	if current_health <= 0:
+		die()
+
+func update_health_bar():
+	vida_inimigo.value = current_health
+	update_health_bar_color()
+
+func update_health_bar_color():
+	var health_percentage = float(current_health) / float(max_health)
+	if health_percentage > 0.6:
+		vida_inimigo.get("theme_override_styles/fill").bg_color = Color.GREEN
+	elif health_percentage > 0.3:
+		vida_inimigo.get("theme_override_styles/fill").bg_color = Color.YELLOW
+	else:
+		vida_inimigo.get("theme_override_styles/fill").bg_color = Color.RED
+
+func die():
+	print("Inimigo morreu!")
+	##colocar animaçao de morte depois
+	queue_free() 
+
+func _on_hurt_box_area_entered(area):
+	if area.is_in_group("socao"):  
+		print("acertou")
+		take_damage(damage_amount)
--- lurdes.gd
+++ lurdes.gd
@@ -1,19 +1,52 @@
 extends CharacterBody2D
-
+@onready var anima = $AnimationPlayer
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var oxigenio = $ProgressBar
+var tempo_regeneracao = 0.0
+var intervalo_regeneracao = 0.8  ##intervalo da regeneraçao de stamina
+
 func _ready():
 	anim.animation_finished.connect(_on_animation_finished)
+	
 
 func _physics_process(delta: float) -> void:
-	pass
 	move_and_slide()
+	
+	##regeneracao de stamina
+	tempo_regeneracao += delta
+	if tempo_regeneracao >= intervalo_regeneracao:
+		tempo_regeneracao = 0.0
+		regenerar_folego(10)  ##regenera 10% a cada segundo
 
 func _input(event):
 	if event.is_action_pressed("tiro") and anim.animation != "direto":
-		anim.play("direto")
+		if oxigenio.value > 0:
+			anim.play("direto")
+			anima.play("porrada")
+			# Reduz 10% do folego
+			reduzir_folego(10)
+		else:
+			print("sem folego")
 	elif event.is_action_pressed("socojab") and anim.animation != "jab":
-		anim.play("jab")
+		##verifica se tem folego
+		if oxigenio.value > 0:
+			anim.play("jab")
+			anima.play("porrada")
+			##reduz 10% do folego
+			reduzir_folego(10)
+		else:
+			print("sem folego")
+
+##reduzir o folego
+func reduzir_folego(percentual: float):
+	var reducao = oxigenio.max_value * (percentual / 100.0)
+	oxigenio.value = max(0, oxigenio.value - reducao)
+
+##Funcao para recuperar o folego
+func regenerar_folego(percentual: float):
+	var regeneracao = oxigenio.max_value * (percentual / 100.0)
+	oxigenio.value = min(oxigenio.max_value, oxigenio.value + regeneracao)
 
 func _on_animation_finished():
 	if anim.animation == "direto" or anim.animation == "jab":
-		anim.play("idle") 
+		anim.play("idle")
